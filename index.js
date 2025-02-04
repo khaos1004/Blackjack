50e43fe8f3e4584d73a081b36fde25a0076c4b74
sunghyun JE <khaos1004@naver.com>
2025-02-04 11:24:20 +0900

    fix

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ const axios = require('axios'); // axios 추가
 // 🔹 SSL 인증서 파일 로드
 const options = {
   key: fs.readFileSync('/opt/game/black/Blackjack/assets/ssl/KeyFile_Wildcard.sotong.com_pem.key'),
-  
-  cert: fs.readFileSync('/opt/game/black/Blackjack/assets/ssl/fullchain.pem'),
-};
+  cert: fs.readFileSync('/opt/game/black/Blackjack/assets/ssl/Wildcard.sotong.com_pem.pem'),
+}
 
 const server = http.createServer(options, app);
 const { Server } = require("socket.io");
